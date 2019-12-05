838bb9e23a73c13836c20f5049b81c36260e77db
Tualatrix Chou <tualatrix@gmail.com>
2019-12-05 11:56:35 +0800

    Rename project to v-run

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.rst", "r") as f:
     long_description = f.read()
 
 setup(
-    name='venv-run',
+    name='v-run',
     version='0.0.0',
     author='Gustavo José de Sousa',
     author_email='gustavo.jo.sousa@gmail.com',
     description='Run commands using Python virtual environment',
     long_description=long_description,
     long_description_content_type='text/x-rst',
-    url='https://github.com/guludo/venv-run',
+    url='https://github.com/tualatrix/v-run',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
@@ -22,11 +22,14 @@ setup(
         'Topic :: Utilities',
     ],
     package_dir={'': 'src'},
-    py_modules=['venvrun'],
+    py_modules=['vrun'],
+    install_requires=[
+        'pexpect',
+    ],
     python_requires='~=3.5',
     entry_points={
         'console_scripts': [
-            'venv-run = venvrun:run',
+            'v-run = vrun:run',
         ]
     },
 )
